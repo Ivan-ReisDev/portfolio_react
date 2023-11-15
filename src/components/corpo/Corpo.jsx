f61d78d6cde855192bb3b971bf3b45d19686f282
Ivan Reis <ivanreisdev@gmail.com>
2023-11-15 00:07:45 -0300

    Atualização de pop-up

--- src/components/corpo/Corpo.jsx
+++ src/components/corpo/Corpo.jsx
@@ -1,5 +1,6 @@
 import Popup_cookies from '../popup_cookies/Popup_cookies'
 import Sobre from '../sobre/Sobre'
+import Tecnologias from '../tecnologias/Tecnologias'
 import './corpo.css'
 
 // eslint-disable-next-line react/prop-types
@@ -10,7 +11,9 @@ const Corpo = ({ modoDark }) => {
     <div className={modoDark ? "corpo" : "dark"}>
 
       <Sobre />
+      <Tecnologias/>
       <Popup_cookies/>
+
     </div>
   )
 }
---
+++ src/components/tecnologias/Tecnologias.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import './tecnologias.css'
+
+
+
+const Tecnologias = () => {
+  return (
+    <>
+        <div className="tecnologias">
+            
+        </div>
+    
+    </>
+  )
+}
+
+export default Tecnologias
