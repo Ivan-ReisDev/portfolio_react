e47af00f281f2b1e32594399259aca901d13db40
Ivan Reis <ivanreisdev@gmail.com>
2023-11-24 19:31:38 -0300

    Atualização responsivo

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -10,17 +10,24 @@ const Nav = () => {
   const eventSidebar = () => { 
     setSidebar(!sidebar)
       }
+
+  const eventSidebarInterno = () => {
+    let width = window.innerWidth
+    if(width <= 850) {
+      setSidebar(!sidebar)
+    }
+  }
   return (
     <>
       <nav> 
          {sidebar ? (<button onClick={eventSidebar} className='btn-sidebar'><FaMixer/></button> ) : 
          (<button onClick={eventSidebar} className='btn-sidebar'><FaBars/></button>)}
               <ul className={sidebar ? "active" : ""} id='navBar'>
-                  <li><a href="home"><span className='icon-nav'><FaHouse/></span>Home</a></li>
-                  <li><a href="#sobre"><span className='icon-nav'><FaUserTie/></span>Sobre</a></li>
-                  <li><a href="#tecnologias"><span className='icon-nav'><FaReact /></span>Tecnologias</a></li>
-                  <li><a href="#projetos"><span className='icon-nav'><IoMdSettings/></span>Projetos</a></li>
-                  <li><a href="Contato"><span className='icon-nav'><FaPhoneAlt /></span>Contato</a></li>
+                  <li onClick={eventSidebarInterno}><a href="home"><span className='icon-nav'><FaHouse/></span>Home</a></li >
+                  <li onClick={eventSidebarInterno}><a href="#sobre"><span className='icon-nav'><FaUserTie/></span>Sobre</a></li>
+                  <li onClick={eventSidebarInterno}><a href="#tecnologias"><span className='icon-nav'><FaReact /></span>Tecnologias</a></li>
+                  <li onClick={eventSidebarInterno}><a href="#projetos"><span className='icon-nav'><IoMdSettings/></span>Projetos</a></li>
+                  <li onClick={eventSidebarInterno}><a href="Contato"><span className='icon-nav'><FaPhoneAlt /></span>Contato</a></li>
               </ul>
           </nav>
     </>
