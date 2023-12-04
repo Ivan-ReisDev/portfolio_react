49ac51036eaebbc07ad6ee2e2e891ae2a8b56188
Ivan Reis <ivanreisdev@gmail.com>
2023-12-04 19:11:16 -0300

    Atualização de links

--- src/components/sobre/Sobre.jsx
+++ src/components/sobre/Sobre.jsx
@@ -17,7 +17,7 @@ const Sobre = () => {
           <div className="paragrafo">
             <h3>Sobre mim</h3>
             <p>Entusiasta de Tecnologia da Informação, estou constantemente em busca de aprimoramento e conhecimento, explorando continuamente novas linguagens. Minha paixão é oferecer o melhor aos meus clientes. Atuo na área desde 2022, especializando-me no desenvolvimento front-end, com expertise em JavaScript, React, Python, e outras tecnologias para criar aplicações web de alta qualidade e valor. Atualmente, estou cursando Análise e Desenvolvimento de Sistemas. Estou pronto e motivado para enfrentar novos desafios.</p>
-            <a href="#" className='links'>Visitar Linkedin <span><FaArrowRight/></span></a>
+            <a href="https://www.linkedin.com/public-profile/settings?trk=d_flagship3_profile_self_view_public_profile"  target='_blank' className='links'>Visitar Linkedin <span><FaArrowRight/></span></a>
           </div>
         </div>
       </section>
